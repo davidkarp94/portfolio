210b277e9ac34cfbde8f590599e2bacbc8771b57
david karp <davidkarp94@gmail.com>
2023-03-30 13:00:42 -0200

    contact left

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,99 @@
 import React from 'react';
 import './portfolio.css';
+import vixbook from '../../assets/redsocial.jpg';
+import bintt from '../../assets/0.jpg';
+import frontendstore from '../../assets/1.jpg';
+import blogdecafe from '../../assets/2.jpg';
+import festivaldemusica from '../../assets/3.jpg';
+import proyectobootstrap from '../../assets/4.jpg';
+import ironvix from '../../assets/5.jpg';
+import proyectocodoacodo from '../../assets/6.jpg';
+
+const data = [
+  {
+    id: 1,
+    image: vixbook,
+    title: 'Social Media Fullstack Vixbook',
+    github: 'https://github.com/davidkarp94/redsocial-fullstack',
+    demo: 'https://vixbook.onrender.com'
+  },
+  {
+    id: 2,
+    image: bintt,
+    title: 'Nft Website (in development)',
+    github: 'https://github.com/m-aragona/bintt-nft-ssr-plugin',
+    demo: 'https://bintt-nft-ssr-plugin-production.up.railway.app'
+  },
+  {
+    id: 3,
+    image: frontendstore,
+    title: 'Front-End Store (Only FrontEnd)',
+    github: 'https://frontend-store-dk.netlify.app',
+    demo: 'https://frontend-store-dk.netlify.app'
+  },
+  {
+    id: 4,
+    image: blogdecafe,
+    title: 'BlogDeCafé (Only FrontEnd)',
+    github: 'https://blog-cafe-dk.netlify.app',
+    demo: 'https://blog-cafe-dk.netlify.app'
+  },
+  {
+    id: 5,
+    image: festivaldemusica,
+    title: 'Rock & EDM Festival (Only FrontEnd)',
+    github: 'https://festival-musica-dk.netlify.app',
+    demo: 'https://festival-musica-dk.netlify.app'
+  },
+  {
+    id: 6,
+    image: proyectobootstrap,
+    title: 'Only Bootstrap Project',
+    github: 'https://github.com/davidkarp94/proyecto-bootstrap',
+    demo: 'https://proyecto-bootstrap-dk.netlify.app'
+  },
+  {
+    id: 7,
+    image: ironvix,
+    title: 'IronVix Pre-Launch Page (Only FrontEnd)',
+    github: 'https://github.com/davidkarp94',
+    demo: 'https://ironvix.com'
+  },
+  {
+    id: 8,
+    image: proyectocodoacodo,
+    title: 'Codo a Codo 4.0 Project (Only FrontEnd)',
+    github: 'https://davidkarp94.github.io/codo-a-codo-tp1/',
+    demo: 'https://davidkarp94.github.io/codo-a-codo-tp1/'
+  },
+
+]
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                <div className="portfolio__item-image">
+                  <img src={image} alt="" />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className='btn' target='_blank'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,10 +1,71 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCube, Pagination } from "swiper";
 import './projects.css';
+import example from '../../assets/0.jpg';
+import "swiper/css";
+import "swiper/css/effect-cube";
+import "swiper/css/pagination";
+
+const data = [
+  {
+    id: 1,
+    image: example,
+    title: 'Project Title',
+    github: 'https://github.com',
+    demo: 'https://github.com'
+  },
+  {
+  id: 2,
+  image: example,
+  title: 'Project Title',
+  github: 'https://github.com',
+  demo: 'https://github.com'
+  }
+]
 
 const Projects = () => {
   return (
-    <section id='projects'>Projects</section>
+    <section id='projects'>
+      <h5>Some small works</h5>
+      <h2>Projects</h2>
+
+      <div className="container projects__container">
+        <Swiper
+          effect={'cube'}
+          grabCursor={true}
+          cubeEffect={{
+            shadow: true,
+            slideShadows: true,
+            shadowOffset: 20,
+            shadowScale: 0.94,
+          }}
+          pagination={true}
+          modules={[EffectCube, Pagination]}
+          className='mySwiper'
+          >
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+            <SwiperSlide key={id}>
+              <article className='project'>
+                <div className="project-image">
+                  <img src={image} alt="" />
+                </div>
+                <h3>{title}</h3>
+                <div className="project-cta">
+                  <a href={github} className='btn' target='_blank'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                </div>
+              </article>
+            </SwiperSlide>
+            )
+          })
+        }
+        </Swiper>
+      </div>
+    </section>
   )
 }
 
-export default Projects
+export default Projects;
