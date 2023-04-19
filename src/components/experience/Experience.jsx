589c29eef19a2b0364e6c712982edfd2546037ab
david karp <davidkarp94@gmail.com>
2023-04-19 16:58:11 -0200

    php

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -94,7 +94,7 @@ const Experience = () => {
               <BsPatchCheckFill className='experience__details-icon' />
               <div>
                 <h4>PHP</h4>
-                <small className='text-light'>Basic</small>
+                <small className='text-light'>Intermediate</small>
               </div>
             </article>
 
